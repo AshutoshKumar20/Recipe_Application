9fe7e3d29ea5c3aaf0141c4321c4d22e6f72d335
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-01-29 13:10:37 +0530

    extractIngredients function is created

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const app_id = "62b8e9d0";
-const app_key = "a0346239bb6b1dd70086069089a17bc7";
+const query = document.querySelector('header.search input')
+const search = document.querySelector('header.search button')
+const cards = document.querySelector('section.cards')
 
-let query = document.querySelector("header.search input");
-let search = document.querySelector("header.search button");
+// `https://www.themealdb.com/api/json/v1/1/search.php?s=${query.value}`
+const extractIngredients = (meal) => {
+    const ingredients = [];
 
-const handleSearch = async () => {
-    try {
-        //const endpoint = `https://api.edamam.com/search?q=${query.value}&app_id=${app_id}&app_key${app_key}`
-        const response = await fetch("http://localhost:5000/recipes?q=")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(err => console.error(err));
+    for (let i = 1; i <= 20; i++) {
+        const ing = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
 
+        if (ing && ing.trim()) {
+            ingredients.push(`${measure} ${ing}`)
+        }
     }
-    catch (error) {
-        console.log(error);
-
-    }
-
+    return ingredients;
 }
-search.addEventListener('click', handleSearch);
